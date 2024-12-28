e427381168151aef5d14ca3ae87a563685a47614
Oray Sebastião <oraymaza@gmail.com>
2024-12-28 10:16:46 -0300

    CRUD scafolding

---
+++ Enums/StatusTarefa.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace keisarmindproject.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description(" Em progresso ")]
+        EmProgresso = 1,
+        [Description(" Agendado ")]
+        Agendado = 2,
+        [Description(" Delegado ")]
+        Delegado = 3,
+        [Description(" Feito ")]
+        Feito = 4,
+        [Description(" Arquivado ")]
+        Arquivado = 5
+    }
+}
